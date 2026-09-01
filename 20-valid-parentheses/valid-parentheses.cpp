b4be89e8d19b1dcd3a6a5ae2c74e2f6cbeaa49ed
tanishqmmm <tanishqmathur93119@gmail.com>
2026-09-01 23:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (89.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++){
+            char c = s[i];
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+
+                if(c == ')' && st.top() == '('){
+                    st.pop();
+                }
+                else if(c == '}' && st.top() == '{'){
+                    st.pop();
+                }
+                else if(c == ']' && st.top() == '['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
